f9ab85e9b79e25945bf666117f73a7d449327836
Robert Elliot <rob@lidalia.org.uk>
2021-02-01 20:22:25 +0000

    Simplify construction of golang template expressions

--- docker-etc-hosts.sh
+++ docker-etc-hosts.sh
@@ -14,18 +14,15 @@ main() {
 get_all_containers() {
 
   # shellcheck disable=SC2016
-  local name_var='{{$name := .Name}}'
-  # shellcheck disable=SC2016
-  local id_var='{{$container_id := .Id}}'
+  local name_var; name_var=$(var_exp 'name' '.Name')
+  local id_var; id_var=$(var_exp 'container_id' '.Id')
 
   local compose_project_var; compose_project_var=$(label_var_default_empty_string 'compose_project' 'com.docker.compose.project')
   local compose_service_var; compose_service_var=$(label_var_default_empty_string 'compose_service' 'com.docker.compose.service')
   local compose_number_var; compose_number_var=$(label_var_default_empty_string 'compose_number' 'com.docker.compose.container-number')
 
-  # shellcheck disable=SC2016
-  local ip_address_var='{{$ip_address := .IPAddress}}'
-  # shellcheck disable=SC2016
-  local network_id_var='{{$network_id := .NetworkID}}'
+  local ip_address_var; ip_address_var=$(var_exp 'ip_address' '.IPAddress')
+  local network_id_var; network_id_var=$(var_exp 'network_id' '.NetworkID')
 
   # shellcheck disable=SC2016
   local entry='{{$name}}|{{$container_id}}|{{$ip_address}}|{{$network_name}}|{{$network_id}}|{{$compose_project}}|{{$compose_service}}|{{$compose_number}}'
@@ -46,23 +43,18 @@ get_all_containers() {
     | sed '/^$/d'
 }
 
+var_exp() {
+  local var_name=$1
+  local expr=$2
+  echo '{{$'"$var_name"' := '"$expr"'}}'
+}
+
 label_var_default_empty_string() {
   local var_name=$1
   local label=$2
-  local label_expr; label_expr="$(label_expression "$label")"
+  local label_expr="index .Config.Labels \"$label\""
 
-  go_template_var_default_empty_string "$var_name" "$label_expr"
-}
-
-go_template_var_default_empty_string() {
-  local var_name=$1
-  local expression=$2
-  echo '{{$'"$var_name"' := "" | or ('"$expression"')}}'
-}
-
-label_expression() {
-  local label=$1
-  echo "index .Config.Labels \"$label\""
+  var_exp "$var_name" '""'" | or ($label_expr)"
 }
 
 all_docker_bridge_networks_as_filter() {
