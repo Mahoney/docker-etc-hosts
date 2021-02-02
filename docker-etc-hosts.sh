2390ab4bd39dffe84a09b56ef9a34f9ca2861fc9
Robert Elliot <rob@lidalia.org.uk>
2021-02-02 12:43:53 +0000

    Move format template into function

--- docker-etc-hosts.sh
+++ docker-etc-hosts.sh
@@ -13,6 +13,15 @@ main() {
 
 get_all_containers() {
 
+  # we want docker ps -q to be expanded
+  # shellcheck disable=SC2046
+  docker inspect \
+    --format="$(format_template)" \
+    $(docker ps -q $(all_docker_bridge_networks_as_filter)) \
+    | sed '/^$/d'
+}
+
+format_template() {
   local name_var; name_var=$(var_exp 'name' '.Name')
   local id_var; id_var=$(var_exp 'container_id' '.Id')
 
@@ -23,20 +32,14 @@ get_all_containers() {
   local ip_address_var; ip_address_var=$(var_exp 'ip_address' '.IPAddress')
   local network_id_var; network_id_var=$(var_exp 'network_id' '.NetworkID')
 
-  local entry; entry=$(join_vars_with_separator '|' 'name' 'container_id' 'ip_address' 'network_name' 'network_id' 'compose_project' 'compose_service' 'compose_number')
+  local entry; entry=$(join_vars_with_separator '|' 'compose_project' 'compose_service' 'compose_number' 'name' 'container_id' 'ip_address' 'network_name' 'network_id')
 
   local per_network="$ip_address_var""$network_id_var""$entry"'{{printf "\n"}}'
 
   # shellcheck disable=SC2016
   local network_loop='{{range $network_name, $value := .NetworkSettings.Networks}}'"$per_network"'{{end}}'
 
-  local format_template="$name_var""$id_var""$compose_project_var""$compose_service_var""$compose_number_var""$network_loop"
-  # we want docker ps -q to be expanded
-  # shellcheck disable=SC2046
-  docker inspect \
-    --format="$format_template" \
-    $(docker ps -q $(all_docker_bridge_networks_as_filter)) \
-    | sed '/^$/d'
+  echo "$name_var""$id_var""$compose_project_var""$compose_service_var""$compose_number_var""$network_loop"
 }
 
 join_vars_with_separator() {
